c7d5e5fc18f976f278c03d87d8a5133b775ad688
BakChanGyu <qmrma987@gmail.com>
2023-03-31 16:00:32 +0900

    insta register

---
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/entity/InstaMember.java
@@ -0,0 +1,29 @@
+package com.ll.gramgram.boundedContext.instaMember.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class InstaMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+    @Column(unique = true)
+    private String username;
+    private String gender;
+}
--- src/test/java/com/ll/gramgram/boundedContext/member/controller/InstaMemberControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/InstaMemberControllerTests.java
@@ -1,27 +1,38 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
-
-import com.ll.gramgram.boundedContext.insta.controller.InstaMemberController;
+import com.ll.gramgram.boundedContext.instaMember.controller.InstaMemberController;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static javax.swing.UIManager.get;
-import static org.springframework.security.config.http.MatcherType.mvc;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest // 스프링부트 관련 컴포넌트 테스트할 때 붙여야함. IoC 컨테이너 작동시킴
 @AutoConfigureMockMvc // http 요청, 응답 테스트를 가능케 해줌.
 @Transactional // 실제로 테스트에서 발생한 DB 작업이 영구적으로 적용되지 않도록 해줌. (test+ transaction 조합은 자동롤백)
 @ActiveProfiles("test") // application-test.yml 을 활성화 시킨다.
 public class InstaMemberControllerTests {
+    @Autowired
+    private InstaMemberService instaMemberService;
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     @DisplayName("인스타회원 정보입력 폼")
@@ -37,6 +48,56 @@ public class InstaMemberControllerTests {
                 .andExpect(handler().handlerType(InstaMemberController.class))
                 .andExpect(handler().methodName("showConnect"))
                 .andExpect(status().is2xxSuccessful())
-                .
+                .andExpect(content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="radio" name="gender" value="W"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="radio" name="gender" value="M"
+                        """.stripIndent().trim())));
+
+    }
+
+    @Test
+    @DisplayName("로그인을 안 하면 인스타 회원 정보 입력 페이지에 접근하면 로그인 페이지로 302")
+    void t002() throws Exception {
+        //WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/instaMember/connect"))
+                .andDo(print());
+
+        //THEN
+        resultActions
+                .andExpect(handler().handlerType(InstaMemberController.class))
+                .andExpect(handler().methodName("showConnect"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/member/login**"));
+    }
+
+    @Test
+    @Rollback(false)
+    @DisplayName("인스타회원 정보입력 폼 처리")
+    @WithUserDetails("user1")
+    void t003() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/instaMember/connect")
+                        .with(csrf())
+                        .param("username", "abc123")
+                        .param("gender", "W")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(InstaMemberController.class))
+                .andExpect(handler().methodName("connect"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("/pop**"));
+
+        InstaMember instaMember = instaMemberService.findByUsername("abc123").orElse(null);
+        assertThat(instaMember).isNotNull();
     }
 }
